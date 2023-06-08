46e89d5b2e9fbda25e747e0935fd10c45ce043a5
Lydia Marks <lydia.marks@alleninstitute.org>
2023-06-08 13:58:29 -0700

    update circle_stim.py script to make it cleaner. remove WhiteCircle.txt file because it's no longer needed to run code.

--- test-scripts/circle_stim.py
+++ test-scripts/circle_stim.py
@@ -18,77 +18,40 @@ monitor = monitors.Monitor("testMonitor", distance=dist, width=wid) #"Gamma1.Lum
 # Create display window
 window = Window(fullscr=True,
                 # monitor= 'Gamma1.Luminance50',          # MS 
-                monitor = monitor,
-                screen=1,
-                warp=Warp.Spherical,)
+               monitor = monitor,
+               screen=0,
+)
 
-
-## Get path of current file
-path = os.path.dirname(os.path.abspath(__file__))
-
-# fl250 = Stimulus.from_file(fl250_path, window) 
-
-## read color information from comma separated file
-with open(path + r"\TestSequenceWhiteCircle.txt") as f:
-    Color = f.readlines()
-    Color = [x.split(',') for x in Color]   
-    Color = [x for x in Color if len(x) > 1]
-    Color = [[y.strip() for y in x] for x in Color]
-    ## remove empty strings
-    Color = [[y for y in x if y] for x in Color]
-    ## convert to float
-    Color = [[float(y) for y in x] for x in Color]
-
-# Create a stimulus using a grating with varying color sweeps 
-fl250 = Stimulus(visual.GratingStim(window,
+# Create a stimulus using a circle mask  
+circle = Stimulus(visual.GratingStim(window,
                     pos=(0, 0),
                     units='deg',
-                    size=80,
+                    size=(80, 80),
                     mask="circle",
                     texRes=256,
-                    # contrast=1,
                     sf=0,
+                    phase = 0, 
                     ),
     sweep_params={
                'Contrast': ([1], 0),
-               'Color':(Color[0], 1)
+                'Color': ([1], 1), 
                },
     sweep_length=0.25,
     start_time=5.0,
     blank_length=4.1,
     blank_sweeps=0,
-    runs=1,
+    runs=120,
     shuffle=False,
     save_sweep_table=True,
     )
 
-# ghg
-# Define display sequence 
-fl250_ds = [(0, 1200)]
-fl250.set_display_sequence(fl250_ds)
-
-# kwargs
-# Set keyword argumets for SweepStim instance 
 params = {
-    'syncpulse': True,
-    'syncpulseport': 1,
-    'syncpulselines': [4, 7],  # frame, start/stop
-    'trigger_delay_sec': 0.0,
-    'bgcolor': (-1,-1,-1),
-    'eyetracker': False,
-    'eyetrackerip': "W7DT12722",
-    'eyetrackerport': 1000,
-    'syncsqr': True,
-    'syncsqrloc': (0,0), 
-    'syncsqrfreq': 60,
-    'syncsqrsize': (100,100),
-    'showmouse': True
-}
 
+}
 
 # Create SweepStim instance
 ss = SweepStim(window,
-               stimuli=[fl250],
+               stimuli=[circle],
                pre_blank_sec=0,
                post_blank_sec=0,
                params=params,
